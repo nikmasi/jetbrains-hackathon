46fd661344f2cf319c4af3e9f14ccd121f3d564e
v-jaroslav <196967611+v-jaroslav@users.noreply.github.com>
2025-12-13 19:17:05 +0100

    Laying foundation for task viewer in plugin

--- tasko-plugin/src/main/kotlin/org/delite/alt/ctrl/taskoplugin/TaskoToolWindow.kt
+++ tasko-plugin/src/main/kotlin/org/delite/alt/ctrl/taskoplugin/TaskoToolWindow.kt
@@ -14,12 +14,12 @@ class TaskoToolWindow : ToolWindowFactory {
     override fun shouldBeAvailable(project: Project) = true
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow()
+        val myToolWindow = TaskoToolComponent()
         val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
         toolWindow.contentManager.addContent(content)
     }
 
-    class MyToolWindow {
+    class TaskoToolComponent {
         private val content = JBPanel<JBPanel<*>>().apply {
             layout = BorderLayout()
 
